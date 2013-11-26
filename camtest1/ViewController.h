eff123a53e730638f4b7ceca64f9aac56cea5228
NeedMoreDesu <desu@horishniy.org.ua>
2013-11-26 17:34:28 +0200

    Initial Commit

---
+++ camtest1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  camtest1
+//
+//  Created by dev on 11/26/13.
+//  Copyright (c) 2013 dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ camtest1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  camtest1
+//
+//  Created by dev on 11/26/13.
+//  Copyright (c) 2013 dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
